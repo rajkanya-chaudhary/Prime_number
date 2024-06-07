c9cb5fd9550451495a13be6393db9649678b1aec
rajkanya-chaudhary <dc5068786@gmail.com>
2024-06-07 23:48:59 +0530

    this is a check vawel String using mathod function

---
+++ Check_vawels.java
@@ -0,0 +1,12 @@
+public class Check_vawels {
+
+    public static void main(String[] args) {
+        String s = "prepinsta";
+        String s1 = "";
+        s1 = s.replaceAll("[aeiou]", ""); 
+        System.out.println("String after removing vowel : "+s1); 
+	}
+
+}
+
+    
