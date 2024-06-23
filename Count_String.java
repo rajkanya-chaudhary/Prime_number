d8d2af496b23bf0645d08f008ba8a074ff3566cb
rajkanya-chaudhary <dc5068786@gmail.com>
2024-06-23 23:16:46 +0530

    this is count String program using

---
+++ Count_String.java
@@ -0,0 +1,24 @@
+public class Count_String {
+    public static int countword(String str)
+    {
+        if(str.length()== 0)
+        {
+            return 0;
+        }
+        int count = 0;
+        for(int i = 0;i<str.length();i++)
+        {
+            if(str.charAt(i)== ' ')
+            {
+                count++;
+            }
+        }
+        return count +1;
+
+    }
+    public static void main(String[] args) {
+        String str = "i am dimpal chaudhary";
+        System.out.println(countword(str));
+
+    }
+}
