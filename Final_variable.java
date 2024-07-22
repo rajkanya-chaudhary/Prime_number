5aef0023d32ac0fb762597de1425c12c9d528779
rajkanya-chaudhary <dc5068786@gmail.com>
2024-07-22 23:15:47 +0530

    this is a final variable program

---
+++ Final_variable.java
@@ -0,0 +1,8 @@
+public class Final_variable {
+    public static void main(String[] args) {
+        final int i = 10;
+              int j  = i + 20;
+                  System.out.println(j);;
+    }
+}
+ 
